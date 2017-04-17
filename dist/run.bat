f3a35e10fe4a7119fe0d36878bad5b040b94478b
Evan Jarrett <ejfirestar00@gmail.com>
2017-04-17 00:56:08 -0700

    create a dist file

---
+++ dist/run.bat
@@ -0,0 +1,4 @@
+@ECHO off
+CHCP 65001 > NUL
+set PYTHONIOENCODING=utf-8
+CMD /k discordfm.exe
--- run.bat
+++ run.bat
@@ -1,4 +1,15 @@
 @ECHO off
 CHCP 65001 > NUL
 set PYTHONIOENCODING=utf-8
-CMD /k discordfm.exe
+
+python --version > NUL 2>&1
+IF %ERRORLEVEL% NEQ 0 GOTO nopython
+
+CMD /k python discordfm.py
+GOTO end
+
+:nopython
+ECHO ERROR: Python has either not been installed or not added to your PATH.
+
+:end
+PAUSE
