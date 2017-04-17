1c7fa3e52a351e9f0968883f9ba0dc61593531ff
Evan J <ejfirestar00@gmail.com>
2017-04-17 00:49:29 -0700

    Add files via upload

--- run.bat
+++ run.bat
@@ -1,15 +1,4 @@
 @ECHO off
 CHCP 65001 > NUL
 set PYTHONIOENCODING=utf-8
-
-python --version > NUL 2>&1
-IF %ERRORLEVEL% NEQ 0 GOTO nopython
-
-CMD /k python discordfm.py
-GOTO end
-
-:nopython
-ECHO ERROR: Python has either not been installed or not added to your PATH.
-
-:end
-PAUSE
+CMD /k discordfm.exe
